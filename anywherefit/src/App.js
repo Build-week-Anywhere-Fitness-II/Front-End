3ec2c8927f56364ceaa628956d5827acd508e574
ZackNemec <zacknemec@live.com>
2020-10-15 18:22:18 -0700

    nothing changed, updating branch

--- anywherefit/src/App.js
+++ anywherefit/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import Login from './components/login';
-import Register from './components/register';
+
 import Routes from './components/routes';
 
 function App() {
