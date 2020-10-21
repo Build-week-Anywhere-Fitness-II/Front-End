fcea3e0f21c357f7150935604fde026954b4638f
ZackNemec <zacknemec@live.com>
2020-10-21 04:18:13 -0700

    search is wokring to show example

--- anywherefit/src/components/InstructorComps/InstructorClassList.js
+++ anywherefit/src/components/InstructorComps/InstructorClassList.js
@@ -6,7 +6,6 @@ const user = window.sessionStorage.getItem('user')
 const filteredArray = props.classes.filter(e => e.instructor_id == user).map((filtered) => {
 return filtered
 })
-
 return (
 <div>
 <Container>
--- anywherefit/src/components/classCard.js
+++ anywherefit/src/components/classCard.js
@@ -1,34 +1,34 @@
 import React from 'react'
-import {Card, Col, CardText, CardTitle, CardSubtitle, Button} from 'reactstrap';
-import {useHistory, Route, useParams, useRouteMatch} from 'react-router-dom';
+import {Card, Col, CardText, CardTitle, CardSubtitle, Button, Container, Row} from 'reactstrap';
+import {useHistory} from 'react-router-dom';
 import CardDetail from './classDetail';
 import ClassDetail from './classDetail';
 
 
 const ClassCard = (props) => {
-  const freeSpots = props.class.max_class_size - props.class.attendees;
-
-  const history = useHistory();
-  console.log(props)
   
-  const classDate = new Date(props.class.class_time);
 
-  console.log(classDate);
-
-    
     return (
+      <Container>
+      <Row>
+        {props.data.map((e) => {
+          const freeSpots = e.max_class_size - e.attendees;
+          return (
         <Col xs="12" sm="6" md="4" lg="3">
-        <Card key={props.class.id}>
-          <CardTitle>{props.class.class_name}</CardTitle>
-    <CardSubtitle>Instructor: {props.class.instructor_id}</CardSubtitle> 
-    {/* Later i will convert id to intructor name*/}
-    <CardSubtitle>Class Time: {props.class.class_time}</CardSubtitle>
-          <CardText>Workout Type: {props.class.type}</CardText>
-          <CardText>Available Spots: {freeSpots}</CardText>
-          <CardText>{props.class.intensity_level}</CardText>
-          <Button onClick={()=>history.push(`/classes/${props.class.id}`)}>Class Details</Button>
+        <Card key={e.id}>
+          <CardTitle>{e.class_name}</CardTitle>
+    <CardSubtitle>Instructor: {e.instructor_id}</CardSubtitle> 
+    <CardSubtitle>Class Time: {e.class_time}</CardSubtitle>
+          <CardText>Workout Type: {e.type}</CardText>
+          <CardText>Available Spots:{freeSpots} </CardText>
+          <CardText> intensity level:{e.intensity_level}</CardText>
+          {/* <Button onClick={()=>history.push(`/classes/${props.data.id}`)}>Class Details</Button> */}
         </Card>
       </Col>
+          );
+        })}
+      </Row>
+      </Container>
     );
 }
 
